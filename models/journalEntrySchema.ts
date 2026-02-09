ba1837ccd4d9ae1f74a7714e6761afe215ba50f5
Lawson Hung <lawsonhung@yahoo.com>
2026-02-08 22:00:33 -0500

    Implement static method JournalEntry.getMoodValues

--- models/journalEntrySchema.ts
+++ models/journalEntrySchema.ts
@@ -21,14 +21,22 @@ const journalEntrySchema = new mongoose.Schema({
   },
   moodValue: {
     type: Number,
-    // Access enum values: const enumValues = MyModel.schema.path('fieldName').options.enum;   
     enum: {
       values: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
       message: '\"{VALUE}\" is not valid. Has to be an integer between 0-10 inclusive.',
     },
     default: 5,
   },
-}, { timestamps: true });
+}, {
+  timestamps: true,
+  statics: {
+    getMoodValues
+  }
+});
+
+function getMoodValues(): [Number] {
+  return journalEntrySchema.path("moodValue").options.enum.values;
+};
 
 export type JournalEntry = InferSchemaType<typeof journalEntrySchema>;
 
